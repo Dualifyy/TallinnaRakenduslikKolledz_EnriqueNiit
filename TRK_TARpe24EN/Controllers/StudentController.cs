b700f340d47cc38aa53a6d3145ed412b021b9939
dsafasdfadf <ligmasmegver@gmail.com>
2025-09-05 11:38:05 +0300

    alelele ahlelallalss

---
+++ TRK_TARpe24EN/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TRK_TARpe24EN.Data;
+namespace TRK_TARpe24EN.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly SchoolContext _context;
+        public StudentController(SchoolContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
