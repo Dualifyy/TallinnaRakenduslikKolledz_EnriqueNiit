431760366b9c6f2427f002faac9e79d326284015
totja <totja@totja.totja>
2025-09-15 14:29:21 +0300

    Instructor Model, InstructorIndexData, OfficeAssignment, CourseAssignment

---
+++ TRK_TARpe24EN/Controllers/InstructorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TRK_TARpe24EN.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TRK_TARpe24EN/Models/OfficeAssignment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TRK_TARpe24EN.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        public int InstructorID { get; set; }
+        public Instructor Instructor { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Kabinet")]
+        public string Location { get; set; }
+    }
+}
