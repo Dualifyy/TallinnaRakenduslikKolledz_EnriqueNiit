7e7809d1420c0e120c6411ba12235841e5dba0fd
totja <totja@totja.totja>
2025-09-15 14:59:11 +0300

    wowwwwТотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️Тотя. ❤️

--- TRK_TARpe24EN/Controllers/InstructorsController.cs
+++ TRK_TARpe24EN/Controllers/InstructorsController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TRK_TARpe24EN.Data;
+using TRK_TARpe24EN.Models;
 
 namespace TRK_TARpe24EN.Controllers
 {
     public class InstructorsController : Controller
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+        public InstructorsController(SchoolContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index( int? id, int? courseId)
+        {
+            var vm = new InstructorIndexData();
+            vm.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)
+                .ToListAsync();
+            return View(vm);
+
         }
     }
 }
