54419eea71ae90f12a4bf6b99cb674658e0da229
opilane <idk@idk.com>
2025-09-18 11:58:36 +0300

    okei dokei

---
+++ TRK_TARpe24EN/Models/AssignedCourseData.cs
@@ -0,0 +1,10 @@
+﻿namespace TRK_TARpe24EN.Models
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+
+    }
+}
