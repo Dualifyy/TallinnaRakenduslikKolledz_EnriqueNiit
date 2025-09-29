6b7c2260ca09cf3dc17e7520f04ce722e2d2e4eb
opilane <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-09-29 14:13:33 +0300

    tesfgsfsrgsgr

--- TRK_TARpe24EN/Controllers/CoursesController.cs
+++ TRK_TARpe24EN/Controllers/CoursesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TRK_TARpe24EN.Data;
+using TRK_TARpe24EN.Models;
 
 namespace TRK_TARpe24EN.Controllers
 {
@@ -17,13 +18,57 @@ namespace TRK_TARpe24EN.Controllers
             var courses = _context.Courses.Include(c => c.Department)
                 .AsNoTracking();
             return View(courses);
-           
+
         }
         [HttpGet]
         public IActionResult Create()
         {
             PopulateDepartmentsDropDownList();
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Course course)
+        {
+            _context.Add(course);
+            await _context.SaveChangesAsync();
+            PopulateDepartmentsDropDownList(course.DepartmentID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+            var courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+            return View(courses);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Courses == null)
+            {
+                return NotFound();
+            }
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                _context.Courses.Remove(course);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
