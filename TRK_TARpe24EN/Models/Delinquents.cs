57e0f7e0532f697c0143f647b4a9c6c47c65b297
opilane <opilane@HP-CZC321705Z>
2025-10-09 12:12:52 +0300

    aedfwadawaef

---
+++ TRK_TARpe24EN/Models/Delinquents.cs
@@ -0,0 +1,14 @@
+﻿namespace TRK_TARpe24EN.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Delinquents
+{
+    [Key]
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public DateTime EventDate { get; set; }
+    public string Violation { get; set; }
+    public string PunishmentType { get; set; }
+    public int PunishmentLength { get; set; }
+
+}
